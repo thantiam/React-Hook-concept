ecd1710fa890e3cf26fdb4c3a1790f4698fe3b40
Thant <adenver.181@gmail.com>
2025-09-19 14:24:03 +0630

    react hook concept

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect, useMemo } from "react";
+
+   function bigFunction() {
+           console.log("Calling some value...");
+          return "some value...";
+      //console.log("React Hook ...");
+   }
+
+export default function App() {
+
+      const [ count, setCount ] = useState(0);
+           
+           const value = useMemo( () => {
+             return bigFunction();
+           }, [] )
+
+        useEffect( () => {
+           bigFunction();
+        }, [] )
+
+              //bigFunction();
+
+           //console.log("React Hook ..."); /** run every time */
+
+  return <div>
+
+           Hello Hook ... <b>( {count} )</b> <br /> <br /> <br />
+
+           <button onClick={ () => setCount(count + 1) } >Increase</button>
+
+           <br /><br /><br />
+
+           <span>{value}</span>
+           
+         </div>
+              
+}
